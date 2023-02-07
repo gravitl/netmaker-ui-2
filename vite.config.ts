36cc3f9c61f732e9dfc8e6865453bf3def7786ca
the_aceix <aceixsmartx@gmail.com>
2023-02-07 11:34:50 +0000

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
