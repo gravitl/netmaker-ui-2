220607a341b4b1ea962aeb8fa84247a84ddb25a6
Aceix <aceixsmartx@gmail.com>
2023-02-10 12:03:08 +0000

    feat(GRA-1076): add single layout structure

--- src/layouts/SingleLayout.tsx
+++ src/layouts/SingleLayout.tsx
@@ -1,1 +1,13 @@
-export default function SingleLayout() {}
+import { Layout } from "antd";
+
+const { Content } = Layout;
+
+export default function SingleLayout() {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Content style={{ margin: "0 16px" }}>
+        
+      </Content>
+    </Layout>
+  )
+}
