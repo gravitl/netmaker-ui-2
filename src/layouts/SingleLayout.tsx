303d5ccfa5de2df6afe72be69c39357a281cb079
the_aceix <aceixsmartx@gmail.com>
2023-02-15 10:59:04 +0000

    fix(GRA-1195): align routing to react-router v6

--- src/layouts/SingleLayout.tsx
+++ src/layouts/SingleLayout.tsx
@@ -1,11 +1,14 @@
 import { Layout } from 'antd';
+import { Outlet } from 'react-router-dom';
 
 const { Content } = Layout;
 
 export default function SingleLayout() {
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Content style={{ margin: '0 16px' }}></Content>
+      <Content style={{ margin: '0 16px' }}>
+        <Outlet />
+      </Content>
     </Layout>
   );
 }
