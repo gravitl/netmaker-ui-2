488b72f675b2970bd84d7c22263b4844244667fa
the_aceix <aceixsmartx@gmail.com>
2023-02-16 11:10:28 +0000

    feat(GRA-1195): switch layout to sidenav dominated

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+import { Outlet } from 'react-router-dom';
+
+const { Content, Footer, Sider } = Layout;
+
+const items: MenuProps['items'] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const SIDE_NAV_EXPANDED_WIDTH = '200px';
+const SIDE_NAV_COLLAPSED_WIDTH = '80px';
+
+export default function MainLayout() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout hasSider>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+        width={SIDE_NAV_EXPANDED_WIDTH}
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        {/* logo */}
+        <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+        <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} items={items} />
+      </Sider>
+      <Layout
+        style={{
+          transition: 'all 200ms',
+          marginLeft: collapsed ? SIDE_NAV_COLLAPSED_WIDTH : SIDE_NAV_EXPANDED_WIDTH,
+        }}
+      >
+        <Content style={{ padding: 24, background: colorBgContainer, overflow: 'initial' }}>
+          <Outlet />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,9 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom';
 import Error404Page from './pages/errors/Error404Page';
 import GettingStartedPage from './pages/GettingStartedPage';
 import DashboardPage from './pages/DashboardPage';
-import { DashboardLayout } from './layouts/DashboardLayout';
+// import { DashboardLayout } from './layouts/DashboardLayout';
 import StartupPage from './pages/StartupPage';
+import MainLayout from './layouts/MainLayout';
 
 export class AppRoutes {
   static DASHBOARD_ROUTE = '/';
@@ -14,7 +15,8 @@ export class AppRoutes {
 const routes: RouteObject[] = [
   {
     path: AppRoutes.DASHBOARD_ROUTE,
-    element: <DashboardLayout />,
+    // element: <DashboardLayout />,
+    element: <MainLayout />,
     children: [{ path: '', element: <DashboardPage /> }],
   },
   { path: AppRoutes.GETTING_STARTED_ROUTE, element: <GettingStartedPage /> },
