165616d2aff7864ff7e74190118843475bc9d3e0
the_aceix <aceixsmartx@gmail.com>
2023-08-04 01:07:01 +0000

    feat(ACC-338): wip - add intercom for SaaS users

--- generate-config.sh
+++ generate-config.sh
@@ -11,10 +11,17 @@ if [ -z "${BACKEND_URL:-}" ]; then
   BACKEND_URL="http://localhost:8081"
 fi
 
+# ensure it is empty string if not set
+if [ -z "${INTERCOM_APP_ID:-}" ]; then
+  INTERCOM_APP_ID=""
+fi
+
 cat >/usr/share/nginx/html/nmui-config.js <<EOF
 window.NMUI_AMUI_URL='$AMUI_URL';
 window.NMUI_BACKEND_URL='$BACKEND_URL';
+window.NMUI_INTERCOM_APP_ID='$INTERCOM_APP_ID';
 EOF
 
 echo ">>>> NMUI_AMUI_URL set to: $AMUI_URL <<<<<"
 echo ">>>> NMUI_BACKEND_URL set to: $BACKEND_URL <<<<<"
+echo ">>>> NMUI_INTERCOM_APP_ID set to: $INTERCOM_APP_ID <<<<<"
