9aae2d45190aa8aeeb54e117bc681996b5c600ce
pwilson77 <p.o.yirenkyi@gmail.com>
2023-09-20 12:05:43 +0000

    feat(NET-501): add horizontal scroll to metrics page

--- scripts/resolve-asset-path.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ "$VITE_IS_SAAS_BUILD" = "true" ]; then
-  sed -i "s/###ASSET_BASE_PATH###/\/v$(jq -r '.version' package.json)/g" vite.config.ts
-  sed -i "s/###ASSET_BASE_PATH###/\/v$(jq -r '.version' package.json)/g" index.html
-else
-  sed -i "s/###ASSET_BASE_PATH###/\//g" vite.config.ts
-  sed -i "s/###ASSET_BASE_PATH###//g" index.html
-fi
