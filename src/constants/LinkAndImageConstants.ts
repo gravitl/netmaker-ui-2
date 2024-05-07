fc4e596770b8e62dcc81f93cf2c446280862bcb1
the_aceix <aceixsmartx@gmail.com>
2024-05-07 14:03:02 +0000

    feat(NET-1134): make sso auth a pro feature

--- src/constants/LinkAndImageConstants.ts
+++ src/constants/LinkAndImageConstants.ts
@@ -1,5 +1,7 @@
 const RAC_LINK = 'https://docs.netmaker.io/pro/rac.html';
 const WIREGUARD_LINK = 'https://docs.netmaker.io/integrating-non-native-devices.html';
+const PRO_UPGRADE_DOCS_LINK = 'https://docs.netmaker.io/pro/pro-setup.html';
+
 const EGRESS_IMG = '/egress.webp';
 const RAG_IMG = '/rag.webp';
 const NET_IMG = '/network.webp';
@@ -7,6 +9,7 @@ const NET_IMG = '/network.webp';
 export const Links = {
   RAC_LINK,
   WIREGUARD_LINK,
+  PRO_UPGRADE_DOCS_LINK,
 };
 
 export const Images = {
