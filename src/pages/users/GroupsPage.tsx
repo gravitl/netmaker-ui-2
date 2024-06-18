600a6aeae2b5024ae33bdc79fb517995200d6617
the_aceix <aceixsmartx@gmail.com>
2024-06-18 15:25:46 +0000

    feat(NET-1331): wip: user mgmt redesign

---
+++ src/pages/users/GroupsPage.tsx
@@ -0,0 +1,5 @@
+import { PageProps } from '@/models/Page';
+
+export default function GroupsPage(props: PageProps) {
+  return <div>GroupsPage</div>;
+}
---
+++ src/pages/users/RolesPage.tsx
@@ -0,0 +1,5 @@
+import { PageProps } from '@/models/Page';
+
+export default function RolesPage(props: PageProps) {
+  return <div>RolesPage</div>;
+}
