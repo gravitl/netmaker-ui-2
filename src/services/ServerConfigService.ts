22fee860cf8801eef67de0c21f1dd773a079ab87
theguy951357 <crispspiceguitar@gmail.com>
2024-07-22 14:39:29 -0400

    update develop to v0.25.0

--- src/services/ServerConfigService.ts
+++ src/services/ServerConfigService.ts
@@ -12,7 +12,7 @@ function getServerStatus() {
 }
 
 /**
- * Obtains the current UI version from package.json. This function returns the semver version prepended with 'v'. Eg: v0.24.4
+ * Obtains the current UI version from package.json. This function returns the semver version prepended with 'v'. Eg: v0.25.0
  * It returns 'latest' if the version field is not specified in package.json
  *
  * @returns current version of UI
