728c5d11a92c6647e9569404ff0a59961d37674c
the_aceix <aceixsmartx@gmail.com>
2024-10-28 13:42:04 +0000

    test(NET-1622): fix ci tests

--- src/constants/Types.ts
+++ src/constants/Types.ts
@@ -75,6 +75,28 @@ export const NULL_NODE: Node = {
     inet_node_client_ids: undefined,
   },
   additional_rag_ips: [],
+  is_static: false,
+  is_user_node: false,
+  static_node: {
+    clientid: '',
+    description: '',
+    privatekey: '',
+    publickey: '',
+    network: '',
+    address: '',
+    address6: '',
+    ingressgatewayid: '',
+    ingressgatewayendpoint: '',
+    lastmodified: 0,
+    enabled: false,
+    ownerid: '',
+    internal_ip_addr: '',
+    internal_ip_addr6: '',
+    dns: '',
+    extraallowedips: [],
+    tags: {},
+  },
+  tags: {},
 };
 
 export const NULL_NETWORK_PROSETTINGS: ProSettings = {
