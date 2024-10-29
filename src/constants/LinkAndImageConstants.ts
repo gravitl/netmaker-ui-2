47908385383993e3048d562e614213d64efa33ab
Imad. <63000523+imadtassaoui@users.noreply.github.com>
2024-10-29 06:43:30 -0700

    Static / Dynamic Nodes (#835)
    * Static / Dynamic Nodes
    
    * Shows External routes comma seperated
    
    * fix: Externallinks and preventing rerender on functions
    
    * fix: net-1646: Add static/dynamic node type to add node dialog
    
    * fix: filters for nodes
    
    * fix: steps for access control
    
    ---------
    
    Co-authored-by: Imad Tassaoui <imadtassaoui@gmail.com>

--- src/constants/LinkAndImageConstants.ts
+++ src/constants/LinkAndImageConstants.ts
@@ -13,14 +13,18 @@ const USER_MGMT_DOCS_URL = 'https://docs.netmaker.io/#';
 const UI_DOCS_URL = 'https://docs.netmaker.io/docs/references/user-interface';
 const USER_MGMT_DOCS_NETWORK_ROLES_URL = 'https://docs.netmaker.io/docs/features/users-management-pro#network-roles';
 const USER_MGMT_DOCS_USER_GROUPS_URL = 'https://docs.netmaker.io/docs/features/users-management-pro#user-groups';
-const USER_MGMT_DOCS_INVITES_URL =
-  'https://docs.netmaker.io/docs/features/users-management-pro#user-groups#adding-users__user-invite';
+const USER_MGMT_DOCS_INVITES_URL = 'https://docs.netmaker.io/docs/features/users-management-pro#user-groups#adding-users__user-invite';
+const APPLE_STORE_LINK = 'https://apps.apple.com/us/app/netmaker-rac/id6479694220?itsct=apps_box_badge&amp;itscg=30200';
+const PLAY_STORE_LINK =
+  'https://play.google.com/store/apps/details?id=com.net.netmaker&pli=1&utm_source=nmui&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1';
+
 
 const EGRESS_IMG = '/egress.webp';
 const RAG_IMG = '/rag.webp';
 const NET_IMG = '/network.webp';
 const WELCOME_IMG = '/welcome-image.webp';
 const RAC_DOWNLOAD_LINK = 'https://www.netmaker.io/download';
+const RAC_DOCS_URL = 'https://docs.netmaker.io/docs/remote-access-client-rac';
 
 export const ExternalLinks = {
   RAC_LINK,
@@ -39,6 +43,9 @@ export const ExternalLinks = {
   USER_MGMT_DOCS_USER_GROUPS_URL,
   USER_MGMT_DOCS_INVITES_URL,
   RAC_DOWNLOAD_LINK,
+  APPLE_STORE_LINK,
+  PLAY_STORE_LINK,
+  RAC_DOCS_URL,
 };
 
 export const AppImages = {
